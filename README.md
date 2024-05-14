e87e19951f659e06d4b9ec1761413955c8962814
gulkullu <wguleren@gmail.com>
2024-05-14 18:56:34 -0400

    son hali 5/14/2024

---
+++ README.md
@@ -0,0 +1,29 @@
+# Salesforce DX Project: Next Steps
+
+Now that you’ve created a Salesforce DX project, what’s next? Here are some documentation resources to get you started.
+
+## How Do You Plan to Deploy Your Changes?
+
+Do you want to deploy a set of changes, or create a self-contained application? Choose a [development model](https://developer.salesforce.com/tools/vscode/en/user-guide/development-models).
+
+## Configure Your Salesforce DX Project
+
+The `sfdx-project.json` file contains useful configuration information for your project. See [Salesforce DX Project Configuration](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_ws_config.htm) in the _Salesforce DX Developer Guide_ for details about this file.
+
+## Read All About It
+
+- [Salesforce Extensions Documentation](https://developer.salesforce.com/tools/vscode/)
+- [Salesforce CLI Setup Guide](https://developer.salesforce.com/docs/atlas.en-us.sfdx_setup.meta/sfdx_setup/sfdx_setup_intro.htm)
+- [Salesforce DX Developer Guide](https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_intro.htm)
+- [Salesforce CLI Command Reference](https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference.htm)
+
+NOTE !!!!
+After you deploy Package.xml, Assign  Home to Home app basics  to your user 
+Two ways to assign permission sets
+1. To scratch org ; Write the following line to Terminal after Package.xml deployment
+sf org assign permset -n Home_to_Home_app_basics
+
+2. Assign from Org 
+GO TO 
+SETUP > PERMISSION SETS > Home to Home app basics > Manage Assignments > Add Assignment > Choose your user checkbox > Next > Assign > Done
+App Manager > Home2Home 
---
+++ force-app/main/default/lwc/americanCuisineLwc/americanCuisineLwc.js
@@ -0,0 +1,69 @@
+import { LightningElement,api, wire, track } from 'lwc';
+import getAmericanCuisineFoods from '@salesforce/apex/AmericanCuisineController.getAmericanCuisineFoods';
+import addToCart from '@salesforce/apex/ShoppingCartController.addToCart';
+
+
+export default class AmericanCuisineLwc extends LightningElement {
+    
+    @track foods = [];
+    @api foodId;
+     quantity = 1; // Default quantity for a combined component scenario
+   
+
+    @wire(getAmericanCuisineFoods)
+    wiredFoods({ error, data }) {
+        if (data) {
+            this.foods = data.map(item => {
+                // Assuming 'Dietary__c' is a comma-separated list
+                const dietaryOptions = item.food.Dietary__c ? item.food.Dietary__c.split(',').map(opt => opt.trim()).join('<br>') : '';
+                return {
+                    ...item,
+                    food: item.food,
+                    dietaryOptions // Updated property with line breaks
+                };
+            });
+        } else if (error) {
+            console.error('Error fetching foods:', error);
+            this.foods = []; 
+        }
+    }
+   
+
+    // Method to handle increasing the quantity
+    handleIncreaseQuantity() {
+        this.quantity += 1; // Increment quantity
+        console.log('Increased Quantity:', this.quantity); // Log to console for debugging
+    }
+
+    // Method to handle decreasing the quantity
+    handleDecreaseQuantity() {
+        if (this.quantity > 1) {
+            this.quantity -= 1; // Decrement quantity
+            console.log('Decreased Quantity:', this.quantity); // Log for debugging
+        }
+    }
+    handleQuantityChange(event) {
+        this.quantity = parseInt(event.target.value, 10); // Update quantity from user input
+    }
+    
+
+    handleAddToCart(event) {
+        const foodId = event.target.dataset.foodId; // Make sure this is correctly set in the HTML as `data-food-id`
+    
+        if (!foodId) {
+            console.error('Food ID is null or undefined');
+            return; // Exit if no foodId is found
+        }
+    
+        addToCart({ foodId: foodId, quantity: this.quantity })
+            .then(result => {
+                console.log('Cart Item Created:', result);
+            })
+            .catch(error => {
+                console.error('Error adding to cart:', error);
+            });
+    }
+    
+}
+
+
